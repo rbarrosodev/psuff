7e19f9baa1ea1494f6320886215358d7a8e86148
Rodrigo Barroso <digorbarroso@gmail.com>
2023-07-10 14:07:16 -0300

    updates init.sql

--- init.sql
+++ init.sql
@@ -5,8 +5,8 @@ CREATE TABLE aeroporto (
     codigo varchar(5) unique not null primary key,
     endereco varchar(100) not null,
     nome varchar(100) unique not null,
-    qtd_pistas integer,
-    qtd_companhias integer,
+    qtdPistas integer,
+    qtdCompanhias integer,
     version integer,
     KEY `id` (`id`) 
 );
@@ -16,7 +16,7 @@ CREATE TABLE terminal (
     numero integer not null,
     aeroporto_id integer,
     foreign key (aeroporto_id) references aeroporto(id),
-    qtd_lojas integer,
+    qtdLojas integer,
     version integer
 );
 
@@ -34,3 +34,21 @@ CREATE TABLE portao (
 INSERT INTO aeroporto (codigo, endereco, nome, qtd_pistas, qtd_companhias, version)
 VALUES ('CGH', 'Av. Washington Luis - Vila Congonhas, Sao Paulo - SP', 'Aeroporto de Sao Paulo/Congonhas', 10, 25, 0);
 
+CREATE DATABASE trabalho6;
+
+use trabalho6;
+
+CREATE TABLE aeroporto (
+    id integer auto_increment primary key,
+    nome varchar(100) unique not null,
+    endereco varchar(100) not null,
+    KEY `id` (`id`) 
+);
+
+CREATE TABLE terminal (
+    id integer auto_increment primary key,
+    numero integer not null,
+    aeroporto_id integer,
+    foreign key (aeroporto_id) references aeroporto(id),
+    qtdLojas integer
+);
